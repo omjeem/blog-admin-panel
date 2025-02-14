a155bab7646a55a326f29d9ca09dfeb978e92cb5
Om Jee Mishra <omjeem558@gmail.com>
2025-02-14 18:50:41 +0530

    blog title length change

--- src/pages/posts/PostList.tsx
+++ src/pages/posts/PostList.tsx
@@ -122,8 +122,8 @@ export default function PostList() {
               {posts.map((post: PostForm) => (
                 <tr key={post._id} className="hover:bg-gray-50">
                   <td className="px-6 py-4 whitespace-nowrap">
-                    <div className="text-sm font-medium text-gray-900">
-                      {post.title}
+                    <div className="text-sm font-medium text-gray-900  ">
+                      {post.title.length > 40 ? post.title.slice(0, 40) + "..." : post.title}
                     </div>
                   </td>
                   <td className="px-6 py-4 whitespace-nowrap">
