d89454e60f2dd867b6e6162c385921a611ae6eb2
Om Jee Mishra <omjeem558@gmail.com>
2025-02-14 18:53:38 +0530

    ui change

--- src/pages/posts/PostList.tsx
+++ src/pages/posts/PostList.tsx
@@ -105,7 +105,7 @@ export default function PostList() {
                   Author
                 </th>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Category
+                  Tags
                 </th>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Status
